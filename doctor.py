d7d15425e8de94d4e4ee93bd5521665cf8304968
lkarjun <lk.arjun@hotmail.com>
2020-09-23 21:21:21 +0530

    Next patient Module created It return next Patient name and op number

--- doctor.py
+++ doctor.py
@@ -7,13 +7,18 @@ class Doctor(OpCounter):
 
     def __init__(self, doctor: str, obj: object) -> None:
         self.doctor = doctor  # Further process dr name is needed
-        self.appointments = OpCounter.display_appointments(obj)
+        self.appointments = [i for i in OpCounter.display_appointments(obj)
+                             if i.Doctor_name == self.doctor]
 
     def total_patients(self) -> Tuple[int, Dict]:
         # Include patients details for respective doctors
-        patients = [i for i in self.appointments
-                    if i.Doctor_name == self.doctor]
+        return len(self.appointments), {patient.Op_number:
+                                        patient.Name
+                                        for patient in self.appointments
+                                        if patient.Doctor_name == self.doctor}
 
-        return len(patients), {patient.Op_number: patient.Name
-                               for patient in patients
-                               if patient.Doctor_name == self.doctor}
+    def next_patient(self) -> Tuple[str, int]:
+        '''It return Next patient Name and Op number'''
+        # Sort by op_number
+        patients = sorted(self.appointments, key=lambda l: l.Op_number)
+        return patients[0].Name, patients[0].Op_number
