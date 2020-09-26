7d7a21ca622532136e47f8f85fe1e3a9c8f17b9e
lkarjun <lk.arjun@hotmail.com>
2020-09-26 10:38:19 +0530

    Securing completed

---
+++ hospital_auth.py
@@ -0,0 +1,41 @@
+from io import open_code
+from cryptography.fernet import Fernet, fernet
+from typing import Union
+import yaml
+
+fileName = 'credential/cred.yaml'
+keyName = 'credential/Key.bin'
+
+
+class Securing:
+
+    def genKey(self):
+        '''Generate key'''
+        key = Fernet.generate_key()
+        with open(keyName, 'wb') as key_to_file:
+            key_to_file.write(key)
+
+    def keyLoading(self) -> Union[bytes, str]:
+        '''load key from file'''
+        return open(keyName, 'rb').read()
+
+    def encrypt_data(self) -> None:
+        '''encrypt the credential file'''
+        key = self.keyLoading()
+        f = Fernet(key)
+        with open(fileName, 'rb') as file:
+            file_data = file.read()
+
+        encrypted_data = f.encrypt(file_data)
+        with open(fileName, 'wb') as file:
+            file.write(encrypted_data)
+
+    def decrypt_data(self) -> None:
+        '''decrypt the credential file'''
+        key = self.keyLoading()
+        f = Fernet(key)
+        with open(fileName, 'rb') as file:
+            encrypted_data = file.read()
+        decrypted_data = f.decrypt(encrypted_data)
+        with open(fileName, 'wb') as file:
+            file.write(decrypted_data)
