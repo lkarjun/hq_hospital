1967d43407bac8fe5c045491acb92fe268ba12b4
lkarjun <lk.arjun@hotmail.com>
2020-10-08 06:41:20 +0530

    Modules for Class_ Medical doctor_receipt_collect ( ) medicine_received ( ) next_op ( )

--- medical_store.py
+++ medical_store.py
@@ -1,5 +1,5 @@
 from typing import List, NamedTuple
-from doctor import Doctor
+from doctor import Doctor, DoctorRemark
 import datetime
 
 
@@ -17,6 +17,28 @@ class MedicalReport(NamedTuple):
 
 
 class Medical(Doctor):
-    def __init__(self) -> None:
+
+    def __init__(self, doctor: Doctor) -> None:
         self.Report: List[MedicalReport] = []
+        self.medicine_received_ops: List = []
         self.Receipt_Number: int = 1000
+        self.obj: Doctor = doctor
+
+    def doctor_receipt_collect(self) -> List[DoctorRemark]:
+        '''It returns full List of remarks of doctor'''
+        return [i
+                for i in self.obj.remark
+                if i.Op_number not in self.medicine_received_ops]
+        
+    def medicine_received(self) -> None:
+        # appends op number to medicine_received_ops
+        op_numb  = self.next_op()
+        self.medicine_received_ops.append(op_numb.Op_number)
+
+    def next_op(self) -> DoctorRemark:
+        '''its return next Patients remarks to receive medicine'''
+        remarks = self.doctor_receipt_collect()
+        return remarks[0]
+
+    def medicine_receipt_create(self) -> None:
+        pass
