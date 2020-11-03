0d1df71cf82833b9982573343760dc10ff56ad6b
lkarjun <lk.arjun@hotmail.com>
2020-11-03 18:07:27 +0530

    Changes in Create_medicine_report

--- medical_store.py
+++ medical_store.py
@@ -1,5 +1,6 @@
 from typing import List, NamedTuple
 from doctor import Doctor, DoctorRemark
+from op_counter import OpCounter
 import datetime
 
 
@@ -40,5 +41,61 @@ class Medical(Doctor):
         remarks = self.doctor_receipt_collect()
         return remarks[0]
 
-    def medicine_receipt_create(self) -> None:
-        pass
+    def create_medicine_report(self) -> None:
+        Receipt: DoctorRemark = self.next_op()
+        report = MedicalReport(ReceiptNumber = self.Receipt_Number,
+                               OpNumber = Receipt.Op_number,
+                               Name = Receipt.Op_detail.Name,
+                               DoctorName = Receipt.Op_detail.Doctor_name,
+                               Remark = Receipt.Remark,
+                               Date = Receipt.Op_detail.Date,
+                               Specialist = Receipt.Op_detail.Specialist,
+                               Patient_email = Receipt.Op_detail.Patient_email)
+        
+        self.Report.append(report)
+
+
+
+# debugging
+if __name__ == '__main__':
+    op = OpCounter()
+    dr = Doctor()
+    dr.set_obj(op)
+    dr.set_doctor_name('Dr Rajesh')
+    op.add_patient(name='Lal', age='19', place='calicut',
+        specialist='Heart', doctor='Dr Rajesh',
+        email='lalu@gmail.com')
+
+    op.add_patient(name='Arun', age='9', place='kochi',
+        specialist='Eye', doctor='Dr Rajesh',
+        email='arun@gmail.com')
+
+    op.add_patient(name='Gopal', age='29', place='palakkad',
+        specialist='Eye', doctor='Dr Rajesh',
+        email='gopal@gmail.com')
+
+    op.add_patient(name='Agitha', age='9', place='kannur',
+        specialist='Eye', doctor='Dr Rajesh',
+        email='agitha@gmail.com')
+
+    op.add_patient(name='Aruthathi', age='4', place='Ernakkulam',
+        specialist='Eye', doctor='Dr Rajesh',
+        email='aruthathi@gmail.com')
+
+    values = ['Not so serious. Medicine Parasitamole', 'Normal bp', 'Imediate blood test',\
+              'Do excerise', 'Do eye focusing excerise']
+
+    for i in range(len(dr.appointments())):
+        dr.add_current_patient()
+        dr.medical(values[i])
+        # print('--------------------------------------')
+        # print('current patient =', dr.current_patient)
+        # print('appointed patients op =', dr.appointed_patients)
+        # print('Remarks\n')
+        dr.remark[i]
+        # print('--------------------------------------')
+    print("_____________");print()
+    medicine__ = Medical(dr)
+    medicine__.next_op()
+    medicine__.create_medicine_report()
+    print(medicine__.Report)
